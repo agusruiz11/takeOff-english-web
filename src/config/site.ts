a03746897e8acc2f556097b16a793e074ca60475
agusruiz11 <agustinmruiz29@gmail.com>
2026-04-28 09:21:03 -0300

    promo banner false

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,7 +2,7 @@ export const siteConfig = {
   calendlyUrl: 'https://calendly.com/gabyteacher428',
   whatsappNumber: '5491179951001',
   whatsappMessage: 'Hola! Quiero agendar mi entrevista gratis para Take Off English 😊',
-  promoEnabled: true,
+  promoEnabled: false,
   promoText: 'Oferta Lanzamiento Febrero y Marzo: 10% de descuento en la compra de un curso completo.',
 };
 
